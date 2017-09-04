82572f33fb79e6c070da3ccef7a6d8b4162fe4c3
artthirt <artthirt@gmail.com>
2017-09-04 06:26:27 +0300

    changed

--- gpu/convnn2_gpu.cpp
+++ gpu/convnn2_gpu.cpp
@@ -542,14 +542,14 @@ bool CnvMomentumOptimizer::pass(std::vector<convnn_gpu> &cnv)
 	m_iteration++;
 	int index = 0;
 	for(convnn_gpu& item: cnv){
-		if(item.use_bn()){
-			if(mG[index].empty()){
-				mG[index].resize(item.bn.dgamma);
-				mB[index].resize(item.bn.dbetha);
-			}
-			momentum_optimizer(item.bn.gamma, mG[index], item.bn.dgamma, m_alpha, m_betha);
-			momentum_optimizer(item.bn.betha, mG[index], item.bn.dbetha, m_alpha, m_betha);
-		}
+//		if(item.use_bn()){
+//			if(mG[index].empty()){
+//				mG[index].resize(item.bn.dgamma);
+//				mB[index].resize(item.bn.dbetha);
+//			}
+//			momentum_optimizer(item.bn.gamma, mG[index], item.bn.dgamma, m_alpha, m_betha);
+//			momentum_optimizer(item.bn.betha, mG[index], item.bn.dbetha, m_alpha, m_betha);
+//		}
 
 		passI(item.gW, item.gB, item.W, item.B, index++);
 	}
