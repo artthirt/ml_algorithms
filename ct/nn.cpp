0fc922309249f5d61fcac486bcd2aecd3fa26307
Zaharov <211_Zaharov@lutch.ru>
2017-09-06 13:42:32 +0300

    fixed

--- ct/nn.cpp
+++ ct/nn.cpp
@@ -11,8 +11,8 @@ void get_cnv_sizes(const ct::Size sizeIn, const ct::Size szW, int stride, ct::Si
 
 void get_cnv_sizes(const Size sizeIn, const Size szW, int stride, Size &szA1)
 {
-	szA1.width		= (sizeIn.width - szW.width) / stride + 1 + (szW.width/2);
-	szA1.height		= (sizeIn.height - szW.height) / stride + 1 + (szW.height/2);
+	szA1.width		= (sizeIn.width - szW.width) / stride + (szW.width/2);
+	szA1.height		= (sizeIn.height - szW.height) / stride + (szW.height/2);
 }
 
 void get_cnv_size_same(const Size szA0, int stride, Size &szA1)
